0d3ae3d06db3de65d6f209ce05007dafdefc0a3b
Raghav Kaushik <136466980+rraghavkaushik@users.noreply.github.com>
2025-03-03 15:05:11 +0530

    Update behaviour_analysis.py

--- Analyser/behaviour_analysis.py
+++ Analyser/behaviour_analysis.py
@@ -30,14 +30,13 @@ import os
 import pandas as pd
 import re
 
-
 def analyse_behaviours(output_folder):
-    
     raster_files = [os.path.join(output_folder, f) for f in os.listdir(output_folder) 
                     if f.startswith('raster') and f.endswith('.xlsx')]
     
     for file_path in raster_files:
         df = pd.read_excel(file_path)
+        
         required_columns = ['Seconds', 'Behaviour']
         if not set(required_columns).issubset(df.columns):
             print(f"Skipping file {file_path}: missing required columns.")
@@ -53,6 +52,7 @@ def analyse_behaviours(output_folder):
             'Locomotion,Others': ['Locomotion,Others']
         }
         
+
         def get_behaviour_category(behaviour):
             for category, keywords in behaviour_map.items():
                 for keyword in keywords:
@@ -60,23 +60,17 @@ def analyse_behaviours(output_folder):
                         return category
             return 'no_detection'
         
+
         df['Behaviour_Cat'] = df['Behaviour'].apply(get_behaviour_category)
         
+
         grouped_3min_df = df.groupby(['serial_3min', 'Behaviour_Cat']).size().reset_index(name='count')
         pivoted_3min_df = grouped_3min_df.pivot(index='serial_3min', columns='Behaviour_Cat', values='count').fillna(0)
         
         grouped_1min_df = df.groupby(['serial_1min', 'Behaviour_Cat']).size().reset_index(name='count')
         pivoted_1min_df = grouped_1min_df.pivot(index='serial_1min', columns='Behaviour_Cat', values='count').fillna(0)
-
-        name = os.path.splitext(os.path.basename(file_path))[0]
-        output_3min_file = os.path.join(output_folder, f"3min-batched-{name}_output.xlsx")
-        pivoted_3min_df.to_excel(output_3min_file, index=True)
         
-        output_1min_file = os.path.join(output_folder, f"1min-batched-{name}_output.xlsx")
-        pivoted_1min_df.to_excel(output_1min_file, index=True)
 
-
-        
         with pd.ExcelWriter(file_path, mode='a', engine='openpyxl') as writer:
             pivoted_3min_df.to_excel(writer, sheet_name='3-min-batching', index=True)
             pivoted_1min_df.to_excel(writer, sheet_name='1-min-batching', index=True)
